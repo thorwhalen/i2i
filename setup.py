4d178948caeb174e9c230d9ebd730c54e8ace83e
thor <thor@otosense.com>
2019-02-12 06:29:58 -0800

    Corrected url of setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='i2i',
       version='0.0.1',
       description='Tools to build adapters for one interface to talk to another',
-      url='http://https://github.com/thorwhalen/i2i.git',
+      url='http://github.com/thorwhalen/i2i.git',
       author='OtoSense',
       license='Apache',
       packages=find_packages())
